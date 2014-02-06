a178a6017e307ace25a157bf033c95cdadb5a826
ribagi <ribagi@live.com>
2014-02-06 00:30:41 -0700

    Calc for Vaults

---
+++ Vault Calc/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vault_Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int size;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)  //Input
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            int Input = Convert.ToInt32(textBox1.Text);
+            double X = Convert.ToDouble(textBox2.Text);
+            double Y = Convert.ToDouble(textBox3.Text);
+
+            Vault vault = new Vault();
+
+            size = vault.vaultVal(Input, X, Y);
+            textBox4.Text = Convert.ToString(size);
+        }
+
+        private void textBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Vault Calc/Vault.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vault_Calc
+{
+    class Vault
+    {
+        public int vaultVal(int layer, double x, double y)
+        {
+            double n = (int)((x / y) * layer);
+            double g = (int)(layer / 5.0);
+
+            double oneoneV = ((2.0 / 3.0) * Math.Pow(n, 3.0)) + (2.0 * Math.Pow(n, 2.0)) + ((7.0 / 3.0) * n);
+            double correction = g * (5.0 + (6.0 * n) + (2.0 * Math.Pow(n, 2.0)));
+            correction = correction + ((Math.Pow(x, 2.0) / 3.0) * g * (g + 1.0) * (2.0 * g + 1.0));
+            correction = correction - (Math.Pow(g, 2.0) + g) * (2.0 * x * n + 3 * x);
+
+            int total = (int)(oneoneV + correction);
+            return total;
+        }
+    }
+}

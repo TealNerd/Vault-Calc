991ee6fcb85f63dfa7a3eb561d516221e375a19d
ribagi <mailiebagi@gmail.com>
2014-11-27 03:40:31 -0700

    New Change

---
+++ Vault Calculator/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vault_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Vault v;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            v = new Vault();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int o = 0;
+            int.TryParse(Layer.Text, out o);
+            v.layer = o;
+
+            v.x = Convert.ToInt32(Head.Text);
+            v.y = Convert.ToInt32(Tail.Text);
+
+            Cost.Text = Convert.ToString(v.Cost());
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Vault Calculator/Vault.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Vault_Calculator
+{
+    class Vault
+    {
+        private int _x;
+        private int _y;
+        private int _layer = 0;
+
+        public int x { set { _x = value; } }
+        public int y { set { _y = value; } }
+        public int layer { set { _layer = value; } }
+
+        public int Cost()
+        {
+            if (_layer < 1)
+            {
+                return 0;
+            }
+            else if (_layer == 1)
+            {
+                return 1;
+            }
+
+            return (int)(((2 * _layer * _layer * _layer) + _layer) / 3.0) + Diff(_layer);
+        }
+
+        public int Diff(int l)
+        {
+            if (l < _y)
+            {
+                return 0;
+            }
+
+            return 2 * (l - _y) * (l - _y) + 2 * (l - _y) + 1 + Diff(l - _x);
+        }
+    }
+}
